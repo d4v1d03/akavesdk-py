087bd092151461a93bda37803c1c49e9f9c083eb
Amit Pandey <amit08072005@gmail.com>
2025-09-01 00:57:39 +0530

    pdpverifier added
    Signed-off-by: Amit Pandey <amit08072005@gmail.com>

--- private/ipc/contracts/__init__.py
+++ private/ipc/contracts/__init__.py
@@ -1,6 +1,7 @@
 from .storage import StorageContract
 from .access_manager import AccessManagerContract
 from .erc1967_proxy import ERC1967Proxy, ERC1967ProxyMetaData, new_erc1967_proxy, deploy_erc1967_proxy
+from .pdp_verifier import PDPVerifier, PDPVerifierMetaData, new_pdp_verifier, deploy_pdp_verifier
 
 __all__ = [
     'StorageContract', 
@@ -8,5 +9,9 @@ __all__ = [
     'ERC1967Proxy',
     'ERC1967ProxyMetaData',
     'new_erc1967_proxy',
-    'deploy_erc1967_proxy'
+    'deploy_erc1967_proxy',
+    'PDPVerifier',
+    'PDPVerifierMetaData',
+    'new_pdp_verifier',
+    'deploy_pdp_verifier'
 ]

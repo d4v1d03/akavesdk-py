4a48902cb350696f873adf72f488c95f3368b289
Raj gill <rajgillproductive@gmail.com>
2025-11-27 17:12:48 +0530

    feat: add httpext HTTP client helper

--- private/httpext/__init__.py
+++ private/httpext/__init__.py
@@ -1,9 +1,5 @@
-"""
-HTTP-related utility functions for internal Akave SDK components.
+# HTTP-related utility functions for internal Akave SDK components.
 
-Currently exposes a `range_download` helper, which mirrors the behaviour of the
-Go `httpext.RangeDownload` function.
-"""
 
 from .httpext import range_download
 
